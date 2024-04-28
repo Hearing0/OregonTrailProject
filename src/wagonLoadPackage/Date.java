379930cb9a0fafbb14422b523e4b0de1c060c62f
Cody Dusek <c-dusek@onu.edu>
2024-04-28 17:04:01 -0400

    removed unneeded methods in Date, began work on Weather

--- src/wagonLoadPackage/Date.java
+++ src/wagonLoadPackage/Date.java
@@ -9,25 +9,17 @@ package wagonLoadPackage;
  */
 public class Date extends Travel {
 
-	
+	// array to list the names of the months in order
 	String[] months = { "January", "February", "March", "April", "May",
 			"June", "July", "August", "September", "October", "November", "December" };
+	
+	// array to list the number of days in each month in order
 	int[] numOfDays = {31,28,31,30,31,30,31,31,30,31,31,31};
 	
 	// date variables for month, day, and year - can be preset if necessary
-	int month = 2;
+	static int month = 0;
 	int day = 0; 
 	int year = 1848;
-
-	/*
-	 * returns the current month as a string
-	 * 
-	 * @return currentMonth - the month as a String
-	 */
-	public String getMonth() {
-		String currentMonth = months[month];
-		return currentMonth;
-	}
 	
 	/**
 	 * checks if the day value is higher than the actual number of days in a month, and increments month and resets day if 0
@@ -39,7 +31,6 @@ public class Date extends Travel {
 			month++;
 			day = 0;
 		}
-		
 		if (month > 11)
 		{
 			month = 0;
@@ -51,7 +42,7 @@ public class Date extends Travel {
 	 * compiles the full date as a String, in the order of month, day, year
 	 * @return - a String consisting of the current date
 	 */
-	public String getDate() {
+	public String getDate(){
 		String suffix = "";
 		String date = months[month];
 		int displayDay = day + 1;
@@ -68,7 +59,7 @@ public class Date extends Travel {
 				suffix = "rd";
 			else
 				suffix = "th";
-		}
+		} 
 		else if (displayDay == 1)
 			suffix = "st";
 		else if (displayDay == 2)
@@ -80,10 +71,8 @@ public class Date extends Travel {
 		
 		date += " " + displayDay + suffix + ", " + year;
 		return date;
-}
+	}
 	
-		
-
 	/**
 	 * increases the day number
 	 */
@@ -96,8 +85,7 @@ public class Date extends Travel {
 	 * sets the month value, planning to be used mainly for when the player begins their journey
 	 * @param month - the month value as a integer, with January being 0 and December being 11
 	 */
-	public void setMonth(int month)
-	{
+	public void setMonth(int month){
 		this.month = month;
 	}
 }
