438e7512fc7a9610121b876f93963bd0dc8ed628
Cody Dusek <c-dusek@onu.edu>
2024-04-28 16:25:03 -0400

    added suffixes to the day in Date

--- src/wagonLoadPackage/Date.java
+++ src/wagonLoadPackage/Date.java
@@ -1,68 +1,39 @@
 package wagonLoadPackage;
 
+/**
+ * Data.java
+ * Version 1.0 - April 28th, 2024
+ * Author - Cody Dusek
+ * 
+ * Simulates time passing as the Oregon Trail is traveled
+ */
 public class Date extends Travel {
 
+	
 	String[] months = { "January", "February", "March", "April", "May",
 			"June", "July", "August", "September", "October", "November", "December" };
-	int month = 2, day = 0, year = 1848;
 	int[] numOfDays = {31,28,31,30,31,30,31,31,30,31,31,31};
+	
+	// date variables for month, day, and year - can be preset if necessary
+	int month = 2;
+	int day = 0; 
+	int year = 1848;
 
-	/**
-	 * determines an actual day value based on the month the user chose to start in,
-	 * and will allow for semi-accurate weather data once implemented
+	/*
+	 * returns the current month as a string
 	 * 
-	 * @param month - the month that the user starts in
+	 * @return currentMonth - the month as a String
 	 */
-	public void setMonth(String monthIn) {
-		if (monthIn == "March") {
-			this.month = 2;
-			this.day = 0;
-		} else if (monthIn == "April") {
-			this.month = 3;
-			this.day = 0;
-		} else if (monthIn == "May") {
-			this.month = 4;
-			this.day = 0;
-		} else if (monthIn == "June") {
-			this.month = 5;
-			this.day = 0;
-		} else if (monthIn == "July") {
-			this.month = 6;
-			this.day = 0;
-		}
-	}
-
 	public String getMonth() {
 		String currentMonth = months[month];
 		return currentMonth;
 	}
-
+	
+	/**
+	 * checks if the day value is higher than the actual number of days in a month, and increments month and resets day if 0
+	 * also does a similar function for the relationship between months and years
+	 */
 	private void setDate() {
-		/**
-		if (month == 2 || month == 4 || month == 6 || month == 7 || month == 9
-				|| month == 11 || month == 0) {
-			if (day > 30) {
-				month++;
-				day = 0;
-			}
-		} else if (month == 1) {
-			if (day > 27) {
-				month++;
-				day = 0;
-			}
-		} else {
-			if (day > 29) {
-				month++;
-				day = 0;
-			}
-		}
-
-		if (month > 11) {
-			month = 0;
-			year++;
-		}
-	}
-	*/
 		if (day > numOfDays[month])
 		{
 			month++;
@@ -76,35 +47,55 @@ public class Date extends Travel {
 		}
 	}
 
+	/**
+	 * compiles the full date as a String, in the order of month, day, year
+	 * @return - a String consisting of the current date
+	 */
 	public String getDate() {
+		String suffix = "";
 		String date = months[month];
 		int displayDay = day + 1;
-		date += " " + displayDay + ", " + year;
-		/**
-		 * TODO implement this later, logic will just take some time
-		 * if (displayDay == 1)
-		 * {
-		 * date += " " + displayDay + "st, ";
-		 * }
-		 * else if (displayDay == 2)
-		 * {
-		 * date += " " + displayDay + "nd, ";
-		 * }
-		 * else if (displayDay == 3)
-		 * {
-		 * date += " " + displayDay + "rd, ";
-		 * }
-		 * date += year;
-		 */
-		System.out.println("Date: " + date);
+	
+		if (displayDay > 30)
+			suffix = "st";
+		else if (displayDay > 20)
+		{
+			if (displayDay == 21)
+				suffix = "st";
+			else if (displayDay == 22)
+				suffix = "nd";
+			else if (displayDay == 23)
+				suffix = "rd";
+			else
+				suffix = "th";
+		}
+		else if (displayDay == 1)
+			suffix = "st";
+		else if (displayDay == 2)
+			suffix = "nd";
+		else if (displayDay == 3)
+			suffix = "rd";
+		else
+			suffix = "th";
+		
+		date += " " + displayDay + suffix + ", " + year;
 		return date;
-	}
+}
+	
+		
 
+	/**
+	 * increases the day number
+	 */
 	public void increaseDays() {
-		day += 5;
+		day++;
 		setDate();
 	}
 	
+	/**
+	 * sets the month value, planning to be used mainly for when the player begins their journey
+	 * @param month - the month value as a integer, with January being 0 and December being 11
+	 */
 	public void setMonth(int month)
 	{
 		this.month = month;
