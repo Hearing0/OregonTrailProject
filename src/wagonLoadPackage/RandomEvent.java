750fd2609a08f2e5565ef357e57c174267a70db5
Brea02 <112592612+Brea02@users.noreply.github.com>
2024-04-28 17:31:26 -0700

    added ArrayList
    made an ArrayList using Events class as the object for eventList
    hopefully formatted in a way that makes adding more events easy
    current methods dont use the arrayList but new methods will be made later once food, health, and weather systems are done

--- src/wagonLoadPackage/RandomEvent.java
+++ src/wagonLoadPackage/RandomEvent.java
@@ -1,35 +1,46 @@
 package wagonLoadPackage;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import javax.swing.JOptionPane;
 
 public class RandomEvent {
 	private static Random rng = new java.util.Random();
+	ArrayList<Events> eventList;
+	String name = "";
+	String locator = "";
 	
 	public RandomEvent(){
+		eventList = new ArrayList<Events>();
+		//will need to adjust the health and other changes once the systems are set up and finalized
+		//should probably make list of locationID needed here and get knowledge from Location class
+		addEvent("Thunderstorm", "General", "", 0, "", 0, false, true);
+		addEvent("Blizzard", "Mountain", "", 0, "", 0, false, true);
+		addEvent("Lost Trail", "General", "", 0, "", 0, true, false);
+		addEvent("Thunderstorm", "General", "", 0, "", 0, false, true);
+		addEvent("A Thief stole from you", "General", "food", -50, "", 0, false, false);
+		addEvent("Wild fruit", "General", "food", 50, "", 0, false, false);
+		addEvent("Injured ox", "General", "", 0, "ox", -50, false, true);
+		addEvent("Bad grass", "General", "", 0, "", 0, false, true);
+		addEvent("Snakebite", "snakeRiver", "", 0, "person", 50, false, false);
+		addEvent("Broken bone", "General", "", 0, "person", -50, false, false);
 		
 	}
 	
+    public void addEvent(String name, String locationID, String whatItem, int itemChange, 
+    		String whoseHealth, int healthChange, boolean affectDate, boolean increaseRisk) {
+    	Events entry = new Events(name, locationID, whatItem, itemChange, whoseHealth, 
+    			healthChange, affectDate, increaseRisk);
+        eventList.add(entry);
+    }
+    //all attributes needed for event explained below
+    //Events(event name, location it occurs at, what item is being changed, 
+    //how much is item being changed, whose health is being changed, 
+	//how much is health being changed by, does it affect the date, does it increase risk)
+
 	
-	//being rewritten under new eventCheck to make it compatible with program
-	//takes total distance and uses a dice like probability counter to determine if event occurs
-	//does it in one fell-swoop, so will need to be changed if dynamic miles are done
-	/*
-	public void activeEvent(int totalDistance) {
-		int milesLeft = totalDistance;
-		while(milesLeft > 0) {
-			int die = rng.nextInt(6) + 1;
-			if(die == 1) {
-				eventAlert();
-				milesLeft = milesLeft - 100;
-			}
-			else {
-				milesLeft = milesLeft - 100;
-			}
-		}
-	}
-	*/
+	
 	
 	//uses rng to randomly check for occurence of random event
 	//basic right now to prepare for weather and traveling changes
@@ -43,7 +54,7 @@ public class RandomEvent {
 		
 	}
 	
-	
+	//will be replaced by the arrayList
 	//determines which event is activated, will need to edit once location and event are relevant
 	//called by the activeEvent method which also does the decrease of miles
 	//displays to console right now, but should probably be put on GUI/pop-box later
@@ -82,7 +93,7 @@ public class RandomEvent {
 		}
 	}
 	
-	
+	//brings up the dialog box that lists the event that occured
 	public void eventPop(String event) {
 		String textF  = event;
 		String titleF = "An event has occured";
