d290e90b5853fed16147d56bb629c94431ea050c
RPiton0814 <143098606+RPiton0814@users.noreply.github.com>
2024-05-04 16:31:31 -0400

    Added event to Mute button/ Volume slider, created array list for items

--- src/wagonLoadPackage/SettingsMenu.java
+++ src/wagonLoadPackage/SettingsMenu.java
@@ -9,11 +9,32 @@ import javax.swing.JCheckBox;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+
 import java.awt.Font;
+import java.awt.Window;
+
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JSlider;
+import javax.swing.event.ChangeListener;
+
+import wagonLoadPackage.Menus.PreLoadingMenu;
+
+import javax.swing.event.ChangeEvent;
+
+
+/**
+ * SettingsMenu.java
+ * Created on 05/01/2024
+ * By Rod Piton
+ * 
+ * Creates the UI for the settings menu. 
+ * Allows user to change volume and start a new game
+ * uses slider to adjust volume
+ * User can also mute volume
+ */
 
 public class SettingsMenu extends JFrame {
 
@@ -23,26 +44,11 @@ public class SettingsMenu extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					SettingsMenu frame = new SettingsMenu();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
+	
 	/**
 	 * Create the frame.
 	 */
-	public SettingsMenu() {
+	public SettingsMenu(Sound sound, JFrame travelMenu) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -54,6 +60,16 @@ public class SettingsMenu extends JFrame {
 		JButton btnNewButton_1 = new JButton("New game");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				
+				JFrame f1 =(JFrame) SwingUtilities.getWindowAncestor(contentPane);
+				 
+				f1.dispose();
+				
+				travelMenu.dispose();
+				
+				PreLoadingMenu frame= new PreLoadingMenu();
+				frame.setVisible(true);
+				
 			}
 		});
 		btnNewButton_1.setBounds(151, 208, 114, 23);
@@ -65,20 +81,36 @@ public class SettingsMenu extends JFrame {
 		lblNewLabel.setBounds(74, 11, 263, 43);
 		contentPane.add(lblNewLabel);
 		
-		JSlider slider = new JSlider();
+		JSlider slider = new JSlider(-40,6);
+		slider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				
+				sound.currentVolume = slider.getValue();
+				
+				if(sound.currentVolume == -40) {
+					sound.currentVolume = -80;
+				}
+				
+				sound.fc.setValue(sound.currentVolume);
+			}
+		});
 		slider.setBounds(106, 91, 200, 26);
 		contentPane.add(slider);
 		
-		JSlider slider_1 = new JSlider();
-		slider_1.setBounds(106, 157, 200, 26);
-		contentPane.add(slider_1);
 		
 		JLabel VolumeLabel = new JLabel("Volume");
 		VolumeLabel.setBounds(189, 65, 76, 14);
 		contentPane.add(VolumeLabel);
 		
-		JLabel BrightnessLabel = new JLabel("Brightness");
-		BrightnessLabel.setBounds(181, 132, 76, 14);
-		contentPane.add(BrightnessLabel);
+		JButton MuteBtn = new JButton("Mute");
+		MuteBtn.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				sound.volumeMute();
+				
+			}
+		});
+		MuteBtn.setBounds(164, 162, 89, 23);
+		contentPane.add(MuteBtn);
 	}
 }
